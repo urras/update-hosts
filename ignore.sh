bfc8d99205ee7532374274e25a8ae7e32ea4057b
urras <an0n102968@riseup.net>
2014-03-10 12:51:12 -0400

    Update ignore.sh

--- ignore.sh
+++ ignore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/gs1.wac.edgecastcdn.net/#gs1.wac.edgecastcdn.net/g' ./hosts/hosts
-sed -i 's/a.facdn.net/#a.facdn.net/g' ./hosts/hosts
+sed -i 's/gs1.wac.edgecastcdn.net/#gs1.wac.edgecastcdn.net/g' ../hosts/hosts
+sed -i 's/a.facdn.net/#a.facdn.net/g' ../hosts/hosts
