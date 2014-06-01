13449e5dcb7829154ea8403f2067207ef6fb0c5e
Urras <urras.hovaker@gmail.com>
2014-06-01 17:38:09 -0400

    Update ignore.sh

--- ignore.sh
+++ ignore.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sed -i 's/gs1.wac.edgecastcdn.net/#gs1.wac.edgecastcdn.net/g' ../hosts/hosts
 sed -i 's/a.facdn.net/#a.facdn.net/g' ../hosts/hosts
+sed -i 's/adf.ly/#adf.ly/g' ../hosts/hosts
