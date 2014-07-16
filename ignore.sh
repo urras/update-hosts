4cb48c0fba49900cd103e871752df69c460c83e3
urras <an0n102968@gmail.com>
2014-07-17 01:41:49 +0200

    New sites in ignore.sh

--- ignore.sh
+++ ignore.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sed -i 's/gs1.wac.edgecastcdn.net/#gs1.wac.edgecastcdn.net/g' /etc/hosts
 sed -i 's/a.facdn.net/#a.facdn.net/g' /etc/hosts
+sed -i 's/adf.ly/#adf.ly/g' /etc/hosts
+sed -i 's/imwx.com/#imwx.com/g' /etc/hosts
+
