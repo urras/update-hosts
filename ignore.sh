4febcdbd7e95c6ae86851b5b9ed05d1e764fddba
Urras <urras.hovaker@gmail.com>
2014-07-29 15:24:50 -0400

    Update ignore.sh

--- ignore.sh
+++ ignore.sh
@@ -4,3 +4,5 @@ sed -i 's/a.facdn.net/#a.facdn.net/g' /etc/hosts
 sed -i 's/adf.ly/#adf.ly/g' /etc/hosts
 sed -i 's/imwx.com/#imwx.com/g' /etc/hosts
 sed -i 's/dw.cbsi.com/#dw.cbsi.com/g' /etc/hosts
+sed -i 's/4.bp.blogspot.com/#4.bp.blogspot.com/g' /etc/hosts
+
